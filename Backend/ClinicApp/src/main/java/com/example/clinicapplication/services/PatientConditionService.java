a835edcd2c6991ad176825e8a8140c8dfeaf099a
Daniel Lozano <danlozano77@gmail.com>
2025-10-23 14:32:14 -0400

    added ConditionService, PatientConditionService, and TrialServices

---
+++ Backend/ClinicApp/src/main/java/com/example/clinicapplication/services/PatientConditionService.java
@@ -0,0 +1,34 @@
+package com.example.clinicapplication.services;
+
+import com.example.clinicapplication.models.PatientCondition;
+import com.example.clinicapplication.repositories.PatientConditionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientConditionService {
+
+    private final PatientConditionRepository patientConditionRepository;
+
+    public PatientConditionService(PatientConditionRepository patientConditionRepository) {
+        this.patientConditionRepository = patientConditionRepository;
+    }
+
+    public List<PatientCondition> getAllPatientConditions() {
+        return patientConditionRepository.findAll();
+    }
+
+    public Optional<PatientCondition> getPatientConditionByIds(int patientId, int conditionId) {
+        return patientConditionRepository.findByPatientIdAndConditionId(patientId, conditionId);
+    }
+
+    public PatientCondition addPatientCondition(PatientCondition pc) {
+        return patientConditionRepository.save(pc);
+    }
+
+    public void deletePatientCondition(int patientId, int conditionId) {
+        patientConditionRepository.deleteByPatientIdAndConditionId(patientId, conditionId);
+    }
+}
